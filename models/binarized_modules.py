c9fb6cdb58149f083657f4a706e209269e8fd315
SDPyle <stevendpyle@gmail.com>
2018-08-03 17:42:33 -0400

    r2

--- models/binarized_modules.py
+++ models/binarized_modules.py
@@ -77,8 +77,8 @@ class BinarizeLinear(nn.Linear):
 
         ## SDPyle modified to maintain consistent positive and negative weights
         ## according to a normal distribution
-        self.real_pos_weights = np.random.normal(1, 0.25, size=self.weight.data.shape)
-        self.real_neg_weights = -1*np.random.normal(1, 0.25, size=self.weight.data.shape)
+        self.real_pos_weights = torch.as_tensor(np.random.normal(1, 0.25, size=self.weight.data.shape))
+        self.real_neg_weights = torch.as_tensor(-1*np.random.normal(1, 0.25, size=self.weight.data.shape))
 
     ## SDPyle modified to include binarization_type for either ideal or with variations
     def forward(self, input):
@@ -99,7 +99,7 @@ class BinarizeLinear(nn.Linear):
         # else:
         self.weight.data = torch.tensor(
             np.where(Binarize(self.weight.org, quant_mode='ge'), self.real_pos_weights, self.real_neg_weights),
-            dtype=torch.cuda.FloatTensor)
+            dtype=torch.FloatTensor)
 
 
         out = nn.functional.linear(input, self.weight)
@@ -116,8 +116,8 @@ class BinarizeConv2d(nn.Conv2d):
 
         ## SDPyle modified to maintain consistent positive and negative weights
         ## according to a normal distribution
-        self.real_pos_weights = np.random.normal(1, 0.25, size=self.weight.data.shape)
-        self.real_neg_weights = -1 * np.random.normal(1, 0.25, size=self.weight.data.shape)
+        self.real_pos_weights = torch.as_tensor(np.random.normal(1, 0.25, size=self.weight.data.shape))
+        self.real_neg_weights = torch.as_tensor(-1 * np.random.normal(1, 0.25, size=self.weight.data.shape))
 
 
     def forward(self, input):
@@ -135,7 +135,7 @@ class BinarizeConv2d(nn.Conv2d):
         # else:
         self.weight.data = torch.tensor(
             np.where(Binarize(self.weight.org, quant_mode='ge'), self.real_pos_weights, self.real_neg_weights),
-            dtype=torch.cuda.FloatTensor)
+            dtype=torch.FloatTensor)
 
         out = nn.functional.conv2d(input, self.weight, None, self.stride,
                                    self.padding, self.dilation, self.groups)
